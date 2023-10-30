04df55c1760b93d5392fd7676ec310ce30d4d917
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-10-30 10:00:06 -0500

    Adds begining server.js functionality

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
