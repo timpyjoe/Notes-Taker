870512fa724eab024e0080ee51112f37edcc95de
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-11-08 22:25:20 -0600

    Adds get and push methods to /api/notes/

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4} = require("uuid");
 
 const PORT = 3001;
 const app = express();
@@ -17,6 +18,32 @@ app.get('/notes', (req, res) => {
 });
 
 
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err){
+      console.log(err);
+    }
+    res.send(JSON.parse(data));
+  })
+})
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = uuidv4();
+  fs.readFile('./db/db.json', (err, data) => {
+    const notesArray = JSON.parse(data);
+    console.log(notesArray);
+    const newNote = req.body;
+    notesArray.push(newNote);
+    console.log(notesArray);
+    fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err) => {
+      if (err){
+        console.log(err)
+      }
+    });
+
+  
+  });
+})
 
 
 
